e5db9b558a86a4c3fadc37a51310b1bde58be318
Avinash Thakur <iamavinashthakur.at@gmail.com>
2019-01-09 01:27:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# robonith.github.io
---
+++ js/projects/readmore.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#toggle").click(function() {
+    var elem = $("#toggle").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#toggle").text("Read Less");
+      $("#text").slideDown();
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#toggle").text("Read More");
+      $("#text").slideUp();
+    }
+  });
+});
