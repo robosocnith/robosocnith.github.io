c4dc99e7cbb617a9511df57845b799b7584eee99
robosocnith <44408751+robosocnith@users.noreply.github.com>
2021-03-20 01:15:34 +0530

    Update custom.js

--- roboweek/assets/js/custom.js
+++ roboweek/assets/js/custom.js
@@ -208,3 +208,19 @@
 
 	
 })(window.jQuery);
+
+
+// Js for counter
+$(document).ready(function(){
+    $('.counter-value').each(function(){
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        },{
+            duration: 3500,
+            easing: 'swing',
+            step: function (now){
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
